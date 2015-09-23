151aa0fd2e907db9c4560b3bcbf7f113f4e0d36d
LeeYouth <yuanliyong520@126.com>
2015-09-23 13:12:05 +0800

    first commit
    first commit.

--- Loadingview/ViewController.m
+++ Loadingview/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //this is test demo
 }
 
 - (void)didReceiveMemoryWarning {
